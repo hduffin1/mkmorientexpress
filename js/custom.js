a73851f5bb6e145bad64db2a3b950ecc4fef1a36
Chris Richardson <chris@true9.co.uk>
2020-03-03 12:39:32 +0000

    Fade in/out footer.

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,14 @@
-$(document).ready(function() {
-	
-	$(".content-holder").css("display", "none");
+$(document).ready(function () {
 
-    $(".content-holder").fadeIn(1500);
-    
-	$("a.transition").click(function(event){
-		event.preventDefault();
-		linkLocation = this.href;
-		$(".content-holder").fadeOut(500, redirectPage);		
-	});
-		
-	function redirectPage() {
-		window.location = linkLocation;
-	}
-	
+    $(".content-holder, footer").css("display", "none");
+    $(".content-holder, footer").fadeIn(1500);
+
+    $("a.transition").click(function (event) {
+        event.preventDefault();
+        var linkLocation = this.href;
+        $(".content-holder, footer").fadeOut(500, function () {
+            window.location = linkLocation;
+        });
+    });
+
 });
